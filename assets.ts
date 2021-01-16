b9543773cc02a6f1c9ece034b377c276460eabaf
Adri314 <34138145+Adri314@users.noreply.github.com>
2021-01-16 14:43:46 -0500

    Initial files for MakeCode project

---
+++ assets.ts
@@ -0,0 +1,13 @@
+namespace spritePackSprites {
+    //% fixedInstance jres blockIdentity=images._image
+    export const mySprite = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const mySprite2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const mySprite3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const mySprite4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const mySprite5 = image.ofBuffer(hex``);
+
+}
